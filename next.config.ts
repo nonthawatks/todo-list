6aefe15b0bebc03a2fff5e23faa143c82e953bb9
nonthawat.k <malassbeats@gmail.com>
2025-02-04 21:19:12 +0700

    #fix: deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
     USER_API: process.env.USER_API,
-  }
+  },
+  output: 'export',
 };
 
 export default nextConfig;
